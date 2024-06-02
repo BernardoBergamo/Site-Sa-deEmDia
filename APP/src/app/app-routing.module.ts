f93b91a4ff6a7c2719346f9685807d4a8d080c27
Andrew Paes <administrator@advincixxi.com.br>
2024-06-02 19:11:03 -0300

    Adicionando página de index

--- APP/src/app/app-routing.module.ts
+++ APP/src/app/app-routing.module.ts
@@ -1,9 +1,9 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { WelcomeComponent } from './components/welcome/welcome.component';
+import { HomeComponent } from './components/home/home.component';
 
 const routes: Routes = [
-  { path: 'welcome', component: WelcomeComponent }
+  { path: '', component: HomeComponent }
 ];
 
 @NgModule({
--- APP/src/app/app.module.ts
+++ APP/src/app/app.module.ts
@@ -3,13 +3,13 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { WelcomeComponent } from './components/welcome/welcome.component';
+import { HomeComponent } from './components/home/home.component';
 import { QRCodeModule } from 'angularx-qrcode';
 
 @NgModule({
   declarations: [
     AppComponent,
-    WelcomeComponent
+    HomeComponent
   ],
   imports: [
     BrowserModule,
